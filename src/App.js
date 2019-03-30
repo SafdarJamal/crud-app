0f2ea087378a175963a6c217ea3569b26b8fd117
Safdar Jamal <cleverprogrammer.me@gmail.com>
2019-03-30 23:56:30 +0500

    First component working fine

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+// import logo from './logo.svg';
+// import { type } from 'os';
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: "Jordon Walke",
+    num_comments: 3,
+    points: 4,
+    objectId: 0
+  }
+];
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list
+    }
+
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  onDismiss(id) {
+    const updatedList = this.state.list.filter(item => {
+      return item.objectId !== id;
+    })
+    this.setState({list: updatedList});
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          {/* <img src={logo} className="App-logo" alt="logo" />
+          <h1>CRUD APP</h1> */}
+          {this.state.list.map(item => (
+            <div key={item.objectId}>
+              <li>
+                <a href={item.url} className="App-link" target="blank">{item.title}</a>
+              </li>
+              <li>{item.author}</li>
+              <li>{item.num_comments}</li>
+              <li>{item.points}</li>
+              <button onClick={() => this.onDismiss(item.objectId)} type="button">Dismiss</button>
+            </div>
+          ))}
         </header>
       </div>
     );
