0795ad94255f47ff0a09ec8620a270f8d1babc12
Safdar Jamal <cleverprogrammer.me@gmail.com>
2019-03-31 02:54:43 +0500

    Login succesfull and tables are rendered

--- src/App.js
+++ src/App.js
@@ -1,78 +1,206 @@
 import React, { Component } from 'react';
+import './primitive.css';
 import './App.css';
-// import logo from './logo.svg';
 // import { type } from 'os';
 
-const list = [
+
+const employeesData = [
   {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: "Jordon Walke",
-    num_comments: 3,
-    points: 4,
-    objectId: 0
+    id: 1,
+    firstName: 'Tania',
+    lastName: 'floppydiskette',
+    email: 'ex@d.com',
+    salary: '30000',
+    date: '10-3-2019'
   },
   {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: "Jordon Walke",
-    num_comments: 3,
-    points: 4,
-    objectId: 1
+    id: 2,
+    firstName: 'Tania',
+    lastName: 'floppydiskette',
+    email: 'ex@d.com',
+    salary: '30000',
+    date: '10-3-2019'
   },
   {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: "Jordon Walke",
-    num_comments: 3,
-    points: 4,
-    objectId: 2
+    id: 3,
+    firstName: 'Tania',
+    lastName: 'floppydiskette',
+    email: 'ex@d.com',
+    salary: '30000',
+    date: '10-3-2019'
   },
   {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: "Jordon Walke",
-    num_comments: 3,
-    points: 4,
-    objectId: 3
-  }
+    id: 4,
+    firstName: 'Tania',
+    lastName: 'floppydiskette',
+    email: 'ex@d.com',
+    salary: '30000',
+    date: '10-3-2019'
+  },
+  {
+    id: 5,
+    firstName: 'Tania',
+    lastName: 'floppydiskette',
+    email: 'ex@d.com',
+    salary: '30000',
+    date: '10-3-2019'
+  },
+  {
+    id: 6,
+    firstName: 'Tania',
+    lastName: 'floppydiskette',
+    email: 'ex@d.com',
+    salary: '30000',
+    date: '10-3-2019'
+  },
+  {
+    id: 7,
+    firstName: 'Tania',
+    lastName: 'floppydiskette',
+    email: 'ex@d.com',
+    salary: '30000',
+    date: '10-3-2019'
+  },
+  {
+    id: 8,
+    firstName: 'Tania',
+    lastName: 'floppydiskette',
+    email: 'ex@d.com',
+    salary: '30000',
+    date: '10-3-2019'
+  },
+  {
+    id: 9,
+    firstName: 'Tania',
+    lastName: 'floppydiskette',
+    email: 'ex@d.com',
+    salary: '30000',
+    date: '10-3-2019'
+  },
+  {
+    id: 10,
+    firstName: 'Tania',
+    lastName: 'floppydiskette',
+    email: 'ex@d.com',
+    salary: '30000',
+    date: '10-3-2019'
+  },
 ];
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      list
-    }
+      email: 'admin@a.com',
+      password: 'asdf',
+      isUser: false
+    };
 
-    this.onDismiss = this.onDismiss.bind(this);
+    // this.adminLogin = this.adminLogin.bind(this);
+    // this.renderLogin = this.renderLogin.bind(this);
+    // this.logout = this.logout.bind(this);
   }
 
-  onDismiss(id) {
-    const updatedList = this.state.list.filter(item => {
-      return item.objectId !== id;
-    })
-    this.setState({list: updatedList});
+  adminLogin() {
+    const {email, password, inputEmail, inputPassword} = this.state;
+    if (inputEmail === email && inputPassword === password) {
+      alert('Admin logged in');
+      console.log('Admin logged in');
+      this.setState({isUser: true})
+    } else {
+      alert("Incorrect crediantials")
+      console.log(this.state.inputEmail, this.state.inputPassword);
+      console.log(this.state);
+    }
+  }
+
+  renderLogin() {
+    return (
+      <div className="container">
+        <form>
+          <h1>Admin login</h1>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            onChange={e => this.setState({ inputEmail: e.target.value })}
+            placeholder="admin@a.com"
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            onChange={e => this.setState({ inputPassword: e.target.value })}
+            placeholder="abcdef"
+          />
+          <input
+            type="button"
+            onClick={() => this.adminLogin()}
+            value="Login"
+          />
+        </form>
+      </div>
+    );
+  }
+
+  employeesTable() {
+    return (
+      <div className="container">
+        <h1>Employees Data</h1>
+        <button className="accent-button">Add</button>
+        <button onClick={() => this.logout()} className="actions">
+          Logout
+        </button>
+        <table>
+          <thead>
+            <tr>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Email</th>
+              <th>Salary</th>
+              <th>Date</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {employeesData.length > 0 ? (
+              employeesData.map(employee => (
+                <tr key={employee.id}>
+                  <td>{employee.firstName}</td>
+                  <td>{employee.lastName}</td>
+                  <td>{employee.email}</td>
+                  <td>{employee.salary}</td>
+                  <td>{employee.date} </td>
+                  <td>
+                    <button className="button muted-button">Edit</button>
+                    <button className="button muted-button actions">
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td colSpan={6}>No users</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+
+  logout() {
+    this.setState({inputEmail: "", inputPassword: "", isUser: false});
+    console.log(this.state);
   }
 
   render() {
+    const { isUser } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" />
-          <h1>CRUD APP</h1> */}
-          {this.state.list.map(item => (
-            <div key={item.objectId}>
-              <li>
-                <a href={item.url} className="App-link" target="blank">{item.title}</a>
-              </li>
-              <li>{item.author}</li>
-              <li>{item.num_comments}</li>
-              <li>{item.points}</li>
-              <button onClick={() => this.onDismiss(item.objectId)} type="button">Dismiss</button>
-            </div>
-          ))}
-        </header>
+      <div>
+        {!isUser && this.renderLogin()}
+        {isUser && this.employeesTable()}
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -10,3 +10,7 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+if (module.hot) {
+  module.hot.accept();
+}
