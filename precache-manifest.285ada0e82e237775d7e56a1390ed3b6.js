08f6f1053781e91db89f8d680dc95fac52fda5e9
Safdar Jamal <cleverprogrammer.me@gmail.com>
2019-04-08 16:48:52 +0500

    Updates

---
+++ precache-manifest.285ada0e82e237775d7e56a1390ed3b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f26f9f9c06b8cbe22dc",
+    "url": "/crud-app/static/js/runtime~main.aac2cc1a.js"
+  },
+  {
+    "revision": "68fcf92373f531aff01c",
+    "url": "/crud-app/static/js/main.066d7837.chunk.js"
+  },
+  {
+    "revision": "378ffc31b6b857dc01df",
+    "url": "/crud-app/static/js/2.a3fb5a04.chunk.js"
+  },
+  {
+    "revision": "68fcf92373f531aff01c",
+    "url": "/crud-app/static/css/main.e4a158d1.chunk.css"
+  },
+  {
+    "revision": "fe02c0dfaa829221f669a0adea8b1507",
+    "url": "/crud-app/index.html"
+  }
+];
