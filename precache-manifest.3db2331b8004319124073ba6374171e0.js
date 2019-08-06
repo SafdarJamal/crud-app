7b12c1ced29bf1de188ca55c8612e3dd858fb559
Safdar Jamal <safdar.dev@gmail.com>
2019-08-06 22:54:40 +0500

    Updates

---
+++ precache-manifest.3db2331b8004319124073ba6374171e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb3ef4ab449b11f8a2a52b81de424cfa",
+    "url": "/crud-app/index.html"
+  },
+  {
+    "revision": "a5537772affc2f603a85",
+    "url": "/crud-app/static/css/main.91bf46a3.chunk.css"
+  },
+  {
+    "revision": "521440421eafe1b9e7c7",
+    "url": "/crud-app/static/js/2.6ba7069a.chunk.js"
+  },
+  {
+    "revision": "a5537772affc2f603a85",
+    "url": "/crud-app/static/js/main.95cd12de.chunk.js"
+  },
+  {
+    "revision": "1f26f9f9c06b8cbe22dc",
+    "url": "/crud-app/static/js/runtime~main.aac2cc1a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/crud-app/precache-manifest.402308a97bc0ae52e48a897894f19231.js"
+  "/crud-app/precache-manifest.3db2331b8004319124073ba6374171e0.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/crud-app/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/crud-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
