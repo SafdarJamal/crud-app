854985475f9413774bac48787823cf0c8d0491c6
Safdar Jamal <safdar.dev@gmail.com>
2020-06-24 20:18:12 +0500

    Create functional Add component with hooks

--- src/components/Dashboard/Add.js
+++ src/components/Dashboard/Add.js
@@ -1,62 +1,46 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Swal from 'sweetalert2';
 
-class Add extends Component {
-  constructor(props) {
-    super(props);
+const Add = ({ employeesData, onAddSuccess, handleAddCancel }) => {
+  const [firstName, setFirstName] = useState(null);
+  const [lastName, setLastName] = useState(null);
+  const [email, setEmail] = useState(null);
+  const [salary, setSalary] = useState(null);
+  const [date, setDate] = useState(null);
 
-    this.state = {
-      firstName: null,
-      lastName: null,
-      email: null,
-      salary: null,
-      date: null
-    };
-
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleAdd = this.handleAdd.bind(this);
-  }
-
-  handleInputChange(propName, e) {
-    this.setState({ [propName]: e.target.value });
-  }
-
-  handleAdd() {
-    const { firstName, lastName, email, salary, date } = this.state;
-    const { employeesData, onAddSuccess } = this.props;
-
+  const handleAdd = () => {
     if (firstName === null) {
       return Swal.fire({
         position: 'center',
-        type: 'error',
+        icon: 'error',
         title: 'All fields are required !',
         showConfirmButton: true
       });
     } else if (lastName === null) {
       return Swal.fire({
         position: 'center',
-        type: 'error',
+        icon: 'error',
         title: 'All fields are required !',
         showConfirmButton: true
       });
     } else if (email === null) {
       return Swal.fire({
         position: 'center',
-        type: 'error',
+        icon: 'error',
         title: 'All fields are required !',
         showConfirmButton: true
       });
     } else if (salary === null) {
       return Swal.fire({
         position: 'center',
-        type: 'error',
+        icon: 'error',
         title: 'All fields are required !',
         showConfirmButton: true
       });
     } else if (date === null) {
       return Swal.fire({
         position: 'center',
-        type: 'error',
+        icon: 'error',
         title: 'All fields are required !',
         showConfirmButton: true
       });
@@ -77,63 +61,56 @@ class Add extends Component {
 
     Swal.fire({
       position: 'center',
-      type: 'success',
+      icon: 'success',
       title: 'New employee added',
       showConfirmButton: false,
       timer: 1500
     });
-  }
+  };
 
-  render() {
-    const { handleAddCancel } = this.props;
-
-    return (
-      <div className="container">
-        <form>
-          <h1>Add Employee</h1>
-          <label htmlFor="fName">First Name</label>
+  return (
+    <div className="container">
+      <form>
+        <h1>Add Employee</h1>
+        <label htmlFor="firstName">First Name</label>
+        <input
+          type="text"
+          id="firstName"
+          onChange={e => setFirstName(e.target.value)}
+        />
+        <label htmlFor="lastName">Last Name</label>
+        <input
+          type="text"
+          id="lastName"
+          onChange={e => setLastName(e.target.value)}
+        />
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          onChange={e => setEmail(e.target.value)}
+        />
+        <label htmlFor="salary">Salary ($)</label>
+        <input
+          type="number"
+          id="salary"
+          onChange={e => setSalary(e.target.value)}
+        />
+        <label htmlFor="date">Date</label>
+        <input type="date" id="date" onChange={e => setDate(e.target.value)} />
+        <div className="main-btns">
+          <input type="button" onClick={handleAdd} value="Add" />
           <input
-            type="text"
-            id="firstName"
-            onChange={e => this.handleInputChange('firstName', e)}
+            type="button"
+            value="Cancel"
+            style={{ marginLeft: '12px' }}
+            className="muted-button"
+            onClick={handleAddCancel}
           />
-          <label htmlFor="lName">Last Name</label>
-          <input
-            type="text"
-            id="lastName"
-            onChange={e => this.handleInputChange('lastName', e)}
-          />
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            id="email"
-            onChange={e => this.handleInputChange('email', e)}
-          />
-          <label htmlFor="salary">Salary ($)</label>
-          <input
-            type="number"
-            id="salary"
-            onChange={e => this.handleInputChange('salary', e)}
-          />
-          <label htmlFor="date">Date</label>
-          <input
-            type="date"
-            id="date"
-            onChange={e => this.handleInputChange('date', e)}
-          />
-          <div className="main-btns">
-            <input type="button" onClick={this.handleAdd} value="Add" />
-            <input
-              type="button"
-              onClick={handleAddCancel}
-              value="Cancel"
-              className="accent-button cancel"
-            />
-          </div>
-        </form>
-      </div>
-    );
-  }
-}
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default Add;
