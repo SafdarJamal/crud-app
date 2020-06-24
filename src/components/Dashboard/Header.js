c49ce032cfb3170ec1fa3495f66e703bdcd7599b
Safdar Jamal <safdar.dev@gmail.com>
2020-06-24 20:19:05 +0500

    Create functional Header component

--- src/components/Dashboard/Header.js
+++ src/components/Dashboard/Header.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Header extends Component {
-  render() {
-    const { handleAddClick, handleLogout } = this.props;
-
-    return (
-      <header>
-        <h1>Employees Data</h1>
-        <div className="main-btns">
-          <button onClick={handleAddClick}>Add Employee</button>
-          <button onClick={handleLogout} className="logout-btn accent-button">
-            Logout
-          </button>
-        </div>
-      </header>
-    );
-  }
-}
+const Header = ({ handleAddClick, handleLogout }) => {
+  return (
+    <header>
+      <h1>Employees Data</h1>
+      <div style={{ marginTop: '30px', marginBottom: '18px' }}>
+        <button onClick={handleAddClick}>Add Employee</button>
+        <button
+          style={{ marginLeft: '12px' }}
+          className="muted-button"
+          onClick={handleLogout}
+        >
+          Logout
+        </button>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
