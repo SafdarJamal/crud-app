01145f45c017f8ad279b191e13050f74934924c6
Safdar Jamal <safdar.dev@gmail.com>
2020-06-24 23:01:09 +0500

    Refactor Edit component

--- src/components/Dashboard/Edit.js
+++ src/components/Dashboard/Edit.js
@@ -10,47 +10,14 @@ const Edit = ({ selectedEmployee, onUpdateSuccess, handleEditCancel }) => {
   const [salary, setSalary] = useState(selectedEmployee.salary);
   const [date, setDate] = useState(selectedEmployee.date);
 
-  const handleUpdate = () => {
-    if (firstName === '') {
+  const handleUpdate = e => {
+    e.preventDefault();
+
+    if (!firstName || !lastName || !email || !salary || !date) {
       return Swal.fire({
-        position: 'center',
         icon: 'error',
-        title: 'All fields are required !',
-        showConfirmButton: true
-      });
-    } else if (lastName === '') {
-      return Swal.fire({
+        title: 'All fields are required!',
         position: 'center',
-        icon: 'error',
-        title: 'All fields are required !',
-        showConfirmButton: true
-      });
-    } else if (email === '') {
-      return Swal.fire({
-        position: 'center',
-        icon: 'error',
-        title: 'All fields are required !',
-        showConfirmButton: true
-      });
-    } else if (email.indexOf(' ') !== -1) {
-      return Swal.fire({
-        position: 'center',
-        icon: 'error',
-        title: 'All fields are required !',
-        showConfirmButton: true
-      });
-    } else if (salary === '') {
-      return Swal.fire({
-        position: 'center',
-        icon: 'error',
-        title: 'All fields are required !',
-        showConfirmButton: true
-      });
-    } else if (date === '') {
-      return Swal.fire({
-        position: 'center',
-        icon: 'error',
-        title: 'All fields are required !',
         showConfirmButton: true
       });
     }
@@ -68,51 +35,56 @@ const Edit = ({ selectedEmployee, onUpdateSuccess, handleEditCancel }) => {
   };
 
   return (
-    <div className="container">
-      <form>
-        <h1>Edit Employee Data</h1>
+    <div className="small-container">
+      <form onSubmit={handleUpdate}>
+        <h1>Edit Employee</h1>
         <label htmlFor="firstName">First Name</label>
         <input
-          type="text"
           id="firstName"
-          defaultValue={firstName}
+          type="text"
+          name="firstName"
+          value={firstName}
           onChange={e => setFirstName(e.target.value)}
         />
         <label htmlFor="lastName">Last Name</label>
         <input
-          type="text"
           id="lastName"
-          defaultValue={lastName}
+          type="text"
+          name="lastName"
+          value={lastName}
           onChange={e => setLastName(e.target.value)}
         />
         <label htmlFor="email">Email</label>
         <input
-          type="email"
           id="email"
-          defaultValue={email}
+          type="email"
+          name="email"
+          value={email}
           onChange={e => setEmail(e.target.value)}
         />
         <label htmlFor="salary">Salary ($)</label>
         <input
-          type="number"
           id="salary"
-          defaultValue={salary}
+          type="number"
+          name="salary"
+          value={salary}
           onChange={e => setSalary(e.target.value)}
         />
         <label htmlFor="date">Date</label>
         <input
-          type="date"
           id="date"
-          defaultValue={date}
+          type="date"
+          name="date"
+          value={date}
           onChange={e => setDate(e.target.value)}
         />
-        <div className="main-btns">
-          <input type="button" onClick={handleUpdate} value="Update" />
+        <div style={{ marginTop: '30px' }}>
+          <input type="submit" value="Update" />
           <input
-            type="button"
-            value="Cancel"
             style={{ marginLeft: '12px' }}
             className="muted-button"
+            type="button"
+            value="Cancel"
             onClick={handleEditCancel}
           />
         </div>
