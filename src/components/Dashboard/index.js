9667200cce9b58c206f5b1b106ec8a9c7022e3f9
Safdar Jamal <safdar.dev@gmail.com>
2020-06-28 02:56:36 +0500

    Refactor Dashboard component

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,129 +1,79 @@
 import React, { useState } from 'react';
 import Swal from 'sweetalert2';
-import { EMPLOYEES_DATA } from '../../data/employees';
 
 import Header from './Header';
 import Table from './Table';
 import Add from './Add';
 import Edit from './Edit';
 
-const Dashboard = ({ onLogoutSuccess }) => {
-  const [employeesData, setEmployeesData] = useState(EMPLOYEES_DATA);
-  const [addClicked, setAddClicked] = useState(false);
-  const [editeClicked, setEditClicked] = useState(false);
+import { employeesData } from '../../data';
+
+const Dashboard = ({ setIsAuthenticated }) => {
+  const [employees, setEmployees] = useState(employeesData);
   const [selectedEmployee, setSelectedEmployee] = useState(null);
+  const [isAdding, setIsAdding] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
 
-  const handleAddClick = () => {
-    setAddClicked(true);
-  };
+  const handleEdit = id => {
+    const [employee] = employees.filter(employee => employee.id === id);
 
-  const handleEditClick = id => {
-    const result = employeesData.filter(employee => employee.id === id);
-
-    const employee = result[0];
-    setEditClicked(true);
     setSelectedEmployee(employee);
-  };
-
-  const handleAddCancel = () => {
-    setAddClicked(false);
-  };
-
-  const handleEditCancel = () => {
-    setEditClicked(false);
-  };
-
-  const onAddSuccess = employeesData => {
-    setEmployeesData(employeesData);
-    setAddClicked(false);
-  };
-
-  const onUpdateSuccess = (id, employeeData) => {
-    employeesData.map((employee, i) => {
-      id === employee.id
-        ? employeesData.splice(i, 1, employeeData)
-        : Swal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            text: 'Something went wrong!'
-          });
-      return false;
-    });
-
-    setEmployeesData(employeesData);
-    setEditClicked(false);
-
-    Swal.fire({
-      position: 'center',
-      icon: 'success',
-      title: `${employeeData.firstName} ${employeeData.lastName} data updated.`,
-      showConfirmButton: false,
-      timer: 1500
-    });
-
-    // console.log(employeesData);
+    setIsEditing(true);
   };
 
   const handleDelete = id => {
     Swal.fire({
+      icon: 'warning',
       title: 'Are you sure?',
       text: "You won't be able to revert this!",
-      icon: 'warning',
       showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Yes, delete it!'
+      confirmButtonText: 'Yes, delete it!',
+      cancelButtonText: 'No, keep it!'
     }).then(result => {
       if (result.value) {
-        const result = employeesData.filter(employee => employee.id === id);
-        const employee = result[0];
+        const [employee] = employees.filter(employee => employee.id === id);
 
         Swal.fire({
-          position: 'center',
           icon: 'success',
           title: 'Deleted!',
-          text: `${employee.firstName} ${employee.lastName} has been deleted.`,
+          text: `${employee.firstName} ${employee.lastName}'s data has been deleted.`,
           showConfirmButton: false,
           timer: 1500
         });
 
-        const updatedList = employeesData.filter(
-          employee => employee.id !== id
-        );
-
-        // console.log(updatedList);
-        setEmployeesData(updatedList);
+        setEmployees(employees.filter(employee => employee.id !== id));
       }
     });
   };
 
   return (
     <div className="container">
-      {!addClicked && !editeClicked && (
-        <Header
-          handleAddClick={handleAddClick}
-          onLogoutSuccess={onLogoutSuccess}
-        />
+      {!isAdding && !isEditing && (
+        <>
+          <Header
+            setIsAdding={setIsAdding}
+            setIsAuthenticated={setIsAuthenticated}
+          />
+          <Table
+            employees={employees}
+            handleEdit={handleEdit}
+            handleDelete={handleDelete}
+          />
+        </>
       )}
-      {!addClicked && !editeClicked && (
-        <Table
-          employeesData={employeesData}
-          handleEditClick={handleEditClick}
-          handleDelete={handleDelete}
-        />
-      )}
-      {addClicked && (
+      {isAdding && (
         <Add
-          employeesData={employeesData}
-          onAddSuccess={onAddSuccess}
-          handleAddCancel={handleAddCancel}
+          employees={employees}
+          setEmployees={setEmployees}
+          setIsAdding={setIsAdding}
         />
       )}
-      {editeClicked && (
+      {isEditing && (
         <Edit
+          employees={employees}
           selectedEmployee={selectedEmployee}
-          onUpdateSuccess={onUpdateSuccess}
-          handleEditCancel={handleEditCancel}
+          setEmployees={setEmployees}
+          setIsEditing={setIsEditing}
         />
       )}
     </div>
